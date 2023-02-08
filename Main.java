43dd0cbc55b5714b7e2e2b1b1c586d00b4b436fc
Armel Dovenon <dove.armel@gmail.com>
2023-02-08 14:33:09 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;   
+
+public class Main {
+     
+    public static void main(String[] args) {
+        int[] tab = {2, 1, 5, 10, 14, 16, 20};
+        Arrays.sort(tab);
+        System.out.println("Array order: " +Arrays.toString(tab)); 
+    }
+}
